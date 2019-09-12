b82f22fd5180d56128e6001dedb0ca5e66b941cc
Bapiro <lucas.rung@gmail.com>
2019-09-12 13:52:32 +0200

    added chess.js functionality

--- js/boardplacement.js
+++ js/boardplacement.js
@@ -1,1 +1,18 @@
-var board1 = Chessboard('board1', 'start');
+//var Chess = require('chess.js').Chess;
+var board, chess = new Chess();
+
+
+
+while (!chess.game_over()) {
+    var moves = chess.moves();
+    var move = moves[Math.floor(Math.random() * moves.length)];
+    chess.move(move);
+}
+console.log(chess.pgn());
+var config = {
+    draggable: true,
+    dropOffBoard: 'trash',
+    position: 'start'
+}
+
+var board = Chessboard('board', config);
