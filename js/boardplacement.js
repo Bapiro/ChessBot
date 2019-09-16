c6860bbc841e4037d2ec365b279741ae1cc76d69
Bapiro <lucas.rung@gmail.com>
2019-09-16 11:19:23 +0200

    added random move from opponent and added only legal moves from player and bot

--- js/boardplacement.js
+++ js/boardplacement.js
@@ -1,18 +1,51 @@
-//var Chess = require('chess.js').Chess;
-var board, chess = new Chess();
+var board = null
+var game = new Chess()
 
+function onDragStart(source, piece, position, orientation) {
+    // do not pick up pieces if the game is over
+    if (game.game_over()) return false
 
-
-while (!chess.game_over()) {
-    var moves = chess.moves();
-    var move = moves[Math.floor(Math.random() * moves.length)];
-    chess.move(move);
+    // only pick up pieces for White
+    if (piece.search(/^b/) !== -1) return false
 }
-console.log(chess.pgn());
+
+function makeRandomMove() {
+    var possibleMoves = game.moves()
+
+    // game over
+    if (possibleMoves.length === 0) return
+
+    var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+    game.move(possibleMoves[randomIdx])
+    board.position(game.fen())
+}
+
+function onDrop(source, target) {
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
+
+    // illegal move
+    if (move === null) return 'snapback'
+
+    // make random legal move for black
+    window.setTimeout(makeRandomMove, 250)
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd() {
+    board.position(game.fen())
+}
+
 var config = {
     draggable: true,
-    dropOffBoard: 'trash',
-    position: 'start'
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd
 }
-
-var board = Chessboard('board', config);
+board = Chessboard('board', config)
