338bb6c2e7161209a2b305a25038ed9e833a9741
Erik Nilsson <eriknilsssonn@gmail.com>
2019-09-23 10:44:37 +0200

    fixed lucas skit

--- js/boardplacement.js
+++ js/boardplacement.js
@@ -1,5 +1,45 @@
 var board = null
 var game = new Chess()
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+console.log("HEJ")
+function removeGreySquares() {
+  $('#board .square-55d63').css('background', '');
+}
+
+function greySquare(square) {
+  var $square = $('#board .square-' + square);
+
+  var background = whiteSquareGrey;
+
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey;
+  }
+
+  $square.css('background', background);
+}
+
+function onMouseOverSquare(square, piece) {
+  // Get list of possible moves
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  });
+
+  // Exit if there are no legal moves for this $square
+  if (moves.length == 0) return;
+  console.log("hej")
+  console.log(square)
+  console.log(piece)
+  // Highlight the possible squares for this moves
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to);
+  }
+}
+
+function onMouseOutSquare(square, piece) {
+  removeGreySquares();
+}
 
 function onDragStart(source, piece, position, orientation) {
     // do not pick up pieces if the game is over
@@ -46,6 +86,12 @@ var config = {
     position: 'start',
     onDragStart: onDragStart,
     onDrop: onDrop,
+    onMouseoverSquare: onMouseOverSquare,
+    onMouseoutSquare: onMouseOutSquare,
     onSnapEnd: onSnapEnd
 }
+<<<<<<< HEAD
 board = Chessboard('board', config);
+=======
+board = Chessboard('board', config)
+>>>>>>> Fixed ome cool colors
