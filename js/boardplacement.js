933767855dae00620ee30b74ffc1d346565eba66
Bapiro <lucas.rung@gmail.com>
2019-09-23 13:25:14 +0200

    added functions so the bot takes pieces on a move before doing empty moves

--- js/boardplacement.js
+++ js/boardplacement.js
@@ -1,44 +1,40 @@
-var board = null
+var chessBoard = null
 var game = new Chess()
 var whiteSquareGrey = '#a9a9a9'
 var blackSquareGrey = '#696969'
-console.log("HEJ")
 function removeGreySquares() {
-  $('#board .square-55d63').css('background', '');
+    $('#chessBoard .square-55d63').css('background', '');
 }
 
 function greySquare(square) {
-  var $square = $('#board .square-' + square);
+    var $square = $('#chessBoard .square-' + square);
 
-  var background = whiteSquareGrey;
+    var background = whiteSquareGrey;
 
-  if ($square.hasClass('black-3c85d')) {
-    background = blackSquareGrey;
-  }
+    if ($square.hasClass('black-3c85d')) {
+        background = blackSquareGrey;
+    }
 
-  $square.css('background', background);
+    $square.css('background', background);
 }
 
 function onMouseOverSquare(square, piece) {
-  // Get list of possible moves
-  var moves = game.moves({
-    square: square,
-    verbose: true
-  });
+    // Get list of possible moves
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    });
 
-  // Exit if there are no legal moves for this $square
-  if (moves.length == 0) return;
-  console.log("hej")
-  console.log(square)
-  console.log(piece)
-  // Highlight the possible squares for this moves
-  for (var i = 0; i < moves.length; i++) {
-    greySquare(moves[i].to);
-  }
+    // Exit if there are no legal moves for this $square
+    if (moves.length == 0) return;
+    // Highlight the possible squares for this moves
+    for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+    }
 }
 
 function onMouseOutSquare(square, piece) {
-  removeGreySquares();
+    removeGreySquares();
 }
 
 function onDragStart(source, piece, position, orientation) {
@@ -52,14 +48,84 @@ function onDragStart(source, piece, position, orientation) {
 function makeRandomMove() {
     var possibleMoves = game.moves();
 
+    //var board = game.SQUARES;
+    //console.log("detta: " + board);
+
     // game over
     if (possibleMoves.length === 0) return;
 
     var randomIdx = Math.floor(Math.random() * possibleMoves.length);
     game.move(possibleMoves[randomIdx]);
-    board.position(game.fen());
+    chessBoard.position(game.fen());
+}
+function getBestMove(gamePlace) {
+    var gameMoves = gamePlace.moves();
+    var bestMove = null;
+    var bestValue = -9999;
+
+
+    for (var i = 0; i < gameMoves.length; i++) {
+        var newGameMove = gameMoves[i];
+        gamePlace.move(newGameMove);
+
+        var boardValue = -evaluateBoard(gamePlace);
+        gamePlace.undo();
+        if (boardValue > bestValue) {
+            bestValue = boardValue;
+            bestMove = newGameMove
+        }
+    }
+    return bestMove;
+
 }
 
+var evaluateBoard = function (gamePlace) {
+    var totalEvaluation = 0;
+    for (var i = 0; i < gamePlace.SQUARES.length; i++) {
+
+        totalEvaluation = totalEvaluation + getPieceValue(gamePlace.get(gamePlace.SQUARES[i]));
+
+    }
+    return totalEvaluation;
+};
+
+var getPieceValue = function (piece) {
+    if (piece === null) {
+        return 0;
+    }
+    var getAbsoluteValue = function (piece) {
+        if (piece.type === 'p') {
+            return 10;
+        } else if (piece.type === 'r') {
+            return 50;
+        } else if (piece.type === 'n') {
+            return 30;
+        } else if (piece.type === 'b') {
+            return 30;
+        } else if (piece.type === 'q') {
+            return 90;
+        } else if (piece.type === 'k') {
+            return 900;
+        }
+        throw "Unknown piece type: " + piece.type;
+    };
+
+    var absoluteValue = getAbsoluteValue(piece, piece.color === 'w');
+    return piece.color === 'w' ? absoluteValue : -absoluteValue;
+};
+
+
+function makeGreatMove() {
+
+    var bestMove = getBestMove(game);
+    game.move(bestMove);
+    chessBoard.position(game.fen());
+    if (game.game_over()) {
+        alert('Game over');
+    }
+
+
+}
 function onDrop(source, target) {
     // see if the move is legal
     var move = game.move({
@@ -72,13 +138,13 @@ function onDrop(source, target) {
     if (move === null) return 'snapback';
 
     // make random legal move for black
-    window.setTimeout(makeRandomMove, 250);
+    window.setTimeout(makeGreatMove, 250);
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd() {
-    board.position(game.fen());
+    chessBoard.position(game.fen());
 }
 
 var config = {
@@ -90,8 +156,4 @@ var config = {
     onMouseoutSquare: onMouseOutSquare,
     onSnapEnd: onSnapEnd
 }
-<<<<<<< HEAD
-board = Chessboard('board', config);
-=======
-board = Chessboard('board', config)
->>>>>>> Fixed ome cool colors
+chessBoard = Chessboard('chessBoard', config);
